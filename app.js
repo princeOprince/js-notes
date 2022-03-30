25f3fe330467694ad0e45bad8786f405c76a1957
Prince Odia <princeodia@gmail.com>
2022-03-30 17:43:09 +0100

    Add bootstrap

--- app.js
+++ app.js
@@ -23,6 +23,9 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use('/assets/vendor/bootstrap', 
+    express.static(path.join(__dirname, 'node_modules', 'bootstrap', 'dist')));
+
 app.use('/', indexRouter);
 // app.use('/users', usersRouter);
 app.use('/notes', notesRouter);
