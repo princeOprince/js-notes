529f0a926e318e9fb08e2f7fdef8d50039ef17a1
Prince Odia <princeodia@gmail.com>
2022-04-07 13:01:05 +0100

    Refactor notes routes to use notes controllers

--- routes/notes.mjs
+++ routes/notes.mjs
@@ -1,78 +1,21 @@
 import express from 'express';
 export const router = express.Router();
-import { NotesStore as notes } from '../app.mjs';
+import * as notesController  from "../controllers/notes.mjs";
 
 //  Add Note (create)
-router.get('/add', (req, res, next) => {
-    res.render('noteedit', {
-        title: "Add a Note", 
-        docreate: true,
-        notekey: "",
-        note: undefined
-    });
-});
+router.get('/add', notesController.addNote);
 
 //  Save Note (create / update)
-router.post('/save', async (req, res, next) => {
-    try {
-        let note;
-        if (req.body.docreate === "create") {
-            note = await notes.create(req.body.notekey, req.body.title, req.body.body);
-        }
-        else {
-            note = await notes.update(req.body.notekey, req.body.title, req.body.body);
-        }
-        res.redirect('/notes/view?key=' + req.body.notekey);
-    } catch (error) {
-        next(err);
-    }
-});
+router.post('/save', notesController.saveNote);
 
 //  Read Note (read)
-router.get('/view', async (req, res, next) => {
-    const note = await notes.read(req.query.key);
-    res.render('noteview', {
-        title: note ? note.title : "",
-        notekey: req.query.key,
-        note: note
-    });
-});
+router.get('/view', notesController.viewNote);
 
 //  Edit Note (update)
-router.get('/edit', async (req, res, next) => {
-    try {
-        const note = await notes.read(req.query.key);
-        res.render('noteedit', {
-            title: note ? ("Edit " + note.title) : "Add a Note",
-            docreate: false,
-            notekey: req.query.key,
-            note: note
-        });
-    } catch (error) {
-        next(error);
-    }
-});
+router.get('/edit', notesController.editNote);
 
 //  Ask to delete Note (destroy)
-router.get('/destroy', async (req, res, next) => {
-    try {
-        const note = await notes.read(req.query.key);
-        res.render('notedestroy', {
-            title: note ? note.title : "",
-            notekey: req.query.key,
-            note: note
-        });
-    } catch (error) {
-        next(error);
-    }
-});
+router.get('/destroy', notesController.destroyNote);
 
 //  Really destroy Note (destroy)
-router.post('/destroy/confirm', async (req, res, next) => {
-    try {
-        await notes.destroy(req.body.notekey);
-        res.redirect('/');
-    } catch (error) {
-        next(error);
-    }
-});
+router.post('/destroy/confirm', notesController.confirmNoteDestroy);
