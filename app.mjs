ecd247338a4827f87bc537879e08fd702c00b183
Prince Odia <princeodia@gmail.com>
2022-04-14 22:41:33 +0100

    Customise bootstrap theme

--- app.mjs
+++ app.mjs
@@ -32,7 +32,7 @@ app.use(cookieParser());
 
 app.use(express.static(path.join(__dirname, 'public')));
 app.use('/assets/vendor/bootstrap', 
-    express.static(path.join(__dirname, 'node_modules', 'bootstrap', 'dist')));
+    express.static(path.join(__dirname, 'theme', 'dist')));
 app.use('/assets/vendor/feather-icons', 
     express.static(path.join(__dirname, 'node_modules', 'feather-icons', 'dist')));
 
