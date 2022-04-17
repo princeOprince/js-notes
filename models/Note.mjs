d83e47bb8dfc5dae2266ef45c3726678c429414e
Prince Odia <princeodia@gmail.com>
2022-04-17 01:33:39 +0100

    Add JSON methods to Note module

--- models/Note.mjs
+++ models/Note.mjs
@@ -14,6 +14,26 @@ export class Note {
     set title(newTitle) { this.#note_title = newTitle; }
     get body() { return this.#note_body; }
     set body(newBody) { this.#note_body = newBody; }
+    get JSON() {
+        return JSON.stringify({
+            key: this.#note_key, title: this.#note_title, body: this.#note_body
+        });
+    }
+
+    static fromJSON(json) {
+        const data = JSON.parse(json);
+        if (typeof data !== 'object'
+            || !data.hasOwnProperty('key')
+            || typeof data.key !== 'string'
+            || !data.hasOwnProperty('title')
+            || typeof data.title !== 'string'
+            || !data.hasOwnProperty('body')
+            || typeof data.body !== 'string') {
+                throw new Error(`Not a Note: ${json}`);
+            }
+        const note = new Note(data.key, data.title, data.body);
+        return note;
+    }
 };
 
 export class AbstractNotesStore {
