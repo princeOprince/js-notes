579d27d423a287357d86282d04f59c50b2b1047f
Prince Odia <princeodia@gmail.com>
2022-04-17 18:11:52 +0100

    Add controller for route index

--- routes/index.mjs
+++ routes/index.mjs
@@ -1,19 +1,7 @@
 import express from 'express';
 export const router = express.Router();
-import { NotesStore as notes } from '../app.mjs';
+import { index } from "../controllers/index.mjs";
 
 /* GET home page. */
-router.get('/', async (req, res, next) => {
-    try {
-        const keylist = await notes.keylist();
-        const keyPromises = keylist.map(key => {
-            return notes.read(key);
-        });
-        const notelist = await Promise.all(keyPromises);
-        res.render('index', { title: 'Notes', notelist: notelist });
-    }
-    catch(err) {
-        next(err);
-    }
-});
+router.get('/', index);
 
