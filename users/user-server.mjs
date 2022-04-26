ac4d38de59d9be3cd7cbbb899f3da1a9d7f63d2c
Prince Odia <princeodia@gmail.com>
2022-04-26 06:56:18 +0100

    Add a REST server for user information

---
+++ users/user-server.mjs
@@ -0,0 +1,68 @@
+import restify from "restify";
+import { 
+    SQUser, connectDB, userParams, findOneUser, createUser, sanitizedUser 
+} from "./users-sequelize.mjs";
+import debug from "debug";
+const log = debug('users:service');
+const error = debug('users:error');
+
+// Set up the REST server
+
+const server = restify.createServer({
+    name: "User-Auth-Service",
+    version: "0.0.1"
+});
+
+server.use(restify.plugins.authorizationParser());
+server.use(check);
+server.use(restify.plugins.queryParser());
+server.use(restify.plugins.bodyParser({
+    mapParams: true
+}));
+
+server.listen(process.env.PORT, 'localhost', () => {
+    log(`${server.name} listening at ${server.url}`);
+});
+
+process.on('uncaughtException', (err) => {
+    error(`Program crashed! \n --- ${(err.stack || err)}`);
+    process.exit(1);
+});
+
+process.on('unhandledRejection', (reason) => {
+    error(`Unhandled rejection at Promise:  \n`,`Error code: ${reason.code} \n`, `Cause ::: ${reason.error}`);
+    process.exit(1);
+});
+
+//  Mimic API Key authentication
+
+const apiKeys = [
+    {
+        user: "them",
+        key: "D4ED43C0-8BD6-4FE2-B358-7C0E230D11EF"
+    }
+];
+
+function check(req, res, next) {
+    if (req.authorization && req.authorization.basic) {
+        let found = false;
+
+        for (let auth of apiKeys) {
+            if (auth.key === req.authorization.basic.password
+                && auth.user === req.authorization.basic.username) {
+                    found = true;
+                    break;
+            }
+        }
+
+        if (found) next();
+        else {
+            res.send(401, new Error('Not authenticated'));
+            next(false);
+        }
+    }
+    else {
+        res.send(500, new Error('No Authorization Key'));
+        next(false);
+    }
+}
