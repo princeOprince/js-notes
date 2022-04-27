dc5f7a5b73ed45ef4264a3209b92983ec5ad0ecb
Prince Odia <princeodia@gmail.com>
2022-04-27 04:20:22 +0100

    Update cli tool to read user data

--- users/cli.mjs
+++ users/cli.mjs
@@ -105,5 +105,27 @@ program
             });
     });
 
+program
+    .command('find <username>')
+    .description('Search for a user on the user server')
+    .action((username, cmdObj) => {
+        client(program).get(`/find/${username}`,
+            (err, req, res, obj) => {
+                if(err) console.error(err.stack);
+                else console.log('Found', obj);
+            });
+    });
+
+program
+    .command('list-users')
+    .description('List all users on the user server')
+    .action((cmdObj) => {
+        client(program).get('/list',
+            (err, req, res, obj) => {
+                if(err) console.error(err.stack);
+                else console.log(obj);
+            });
+    });
+
 
 program.parse(process.argv);
