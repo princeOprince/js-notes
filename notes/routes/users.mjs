5debf1e8893476bb682b431c3c247eaa4b40e647
Prince Odia <princeodia@gmail.com>
2022-05-19 03:05:53 +0100

    Initialise passport module

---
+++ notes/routes/users.mjs
@@ -0,0 +1,27 @@
+import path from "path";
+import express from "express";
+import passport from "passport";
+import { Strategy as LocalStrategy } from "passport-local";
+import * as usersModel from "../models/users-superagent.mjs";
+import { sessionCookieName } from "../app.mjs";
+import debug from "debug";
+const log = debug('notes:router-users');
+const error = debug('notes:error-users');
+
+export const router = express.Router();
+
+export function initPassport(app) {
+  app.use(passport.initialize());
+  app.use(passport.session());
+}
+
+export function ensureAuthenticated(req, res, next) {
+  try {
+    if (req.user) next();
+    else res.redirect('/users/login');
+  }
+  catch (e) {
+    error(e);
+    next(e);
+  }
+}
