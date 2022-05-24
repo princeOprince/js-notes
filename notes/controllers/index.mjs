32e8dd30a12f5086b4f304a6384c27584dc2a735
Prince Odia <princeodia@gmail.com>
2022-05-24 09:27:49 +0100

    Update route index to add user data

--- notes/controllers/index.mjs
+++ notes/controllers/index.mjs
@@ -1,15 +1,24 @@
 import { NotesStore as notes } from '../models/notes-store.mjs';
+import debug from 'debug';
+const log = debug('notes:index-controller');
+const error = debug('notes:error-index-controller');
 
 export const index = async (req, res, next) => {
-    try {
-        const keylist = await notes.keylist();
-        const keyPromises = keylist.map(key => {
-            return notes.read(key);
-        });
-        const notelist = await Promise.all(keyPromises);
-        res.render('index', { title: 'Notes', notelist: notelist });
-    }
-    catch(err) {
-        next(err);
-    }
+  try {
+    const keylist = await notes.keylist();
+    const keyPromises = keylist.map(key => {
+      return notes.read(key);
+    });
+    const notelist = await Promise.all(keyPromises);
+    log(notelist);
+    res.render('index', {
+      title: 'Notes',
+      notelist: notelist,
+      user: req.user ? req.user : undefined
+    });
+  }
+  catch (err) {
+    error(err);
+    next(err);
+  }
 }
